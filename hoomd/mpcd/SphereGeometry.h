a1a301dd615c1958e2539d187c268faffd362a63
YMWani <ymanishw@uni-mainz.de>
2021-11-23 00:54:27 +0100

    changes after review 2

--- hoomd/mpcd/SphereGeometry.h
+++ hoomd/mpcd/SphereGeometry.h
@@ -33,7 +33,7 @@ class __attribute__((visibility("default"))) SphereGeometry
          * \param bc Boundary condition at the wall (slip or no-slip)
          */
         HOSTDEVICE SphereGeometry(Scalar R, boundary bc)
-            : m_R(R), m_bc(bc)
+            : m_R(R), m_R2(R*R), m_bc(bc)
             { }
 
         //! Detect collision between the particle and the boundary
@@ -53,29 +53,17 @@ class __attribute__((visibility("default"))) SphereGeometry
 
             /*
              * Detect if particle has left the spherical confinement.
-             * comparison: 'r2 < R*R' used in the following calculations is equal to 0(FALSE) if the particle is outside
+             * comparison: 'r2 <= R*R' used in the following calculations is equal to 0(FALSE) if the particle is outside
              * a sphere of radius R, 1(TRUE) if inside.
-             *
-             * We intentionally use < rather than <= in the comparison so that spurious collisions are not detected
-             * when a particle is reset to the spherical boundary. A particle landing exactly on the spherical boundary
-             * can be reflected in the next streaming step.
-             * TODO: Verify if the comparison can impact the dynamics incorrectly since we aim to have a dynamic spherical confinement.
              */
             Scalar r2 = dot(pos,pos);
-            // exit immediately if no collision is found or particle is not moving normal (radial direction) to the wall
-            // (since no new collision could have occurred if there is no normal motion)
+            // exit immediately if no collision is found.
             /*
-            r(t) = sqrt(x**2 + y**2 + z**2)
-            radial velocity, r_dot = (x*vel_x + y*vel_y + z*vel_z)/r(t)
-
-            define variable vr = dot(pos,vel) to check for radial motion, for computational efficiency.
-
-            To avoid division by zero error later on (line 103), we exit immediately. (no collision could have occurred
+            To avoid division by zero error later on, we exit immediately. (no collision could have occurred
             if the particle speed is equal to zero)
             */
-            Scalar vr = dot(pos,vel);
             Scalar v2 = dot(vel,vel);
-            if (r2 < R*R || vr == Scalar(0) || v2 == Scalar(0))
+            if (r2 <= R2 || v2 == Scalar(0))
                {
                dt = Scalar(0);
                return false;
@@ -90,49 +78,49 @@ class __attribute__((visibility("default"))) SphereGeometry
             *    1. r(t+del_t)      (the point outside the sphere)
             *    2. v(t)            (previous velocity)
             *
-            * Assuming 'r*' is the point of contact on the spherical shell and dx* is the distance travelled by the
-            * particle outside the boundary.
+            * Assuming 'r*' is the point of contact on the spherical shell and dt is the time particle travelled
+            * outside the boundary.
             *
             * r* = r(t+del_t) - dx* * v(t)/|v|
-            * and |r|**2 = R**2
+            * and |r*|**2 = R**2
             *
             * Solving the above equations,
             * dx* = (r.v^) - sqrt((r.v^)**2 - r2 + R2)
             * consequently, dt = dx* / |v|
+            *
+            * --> dt = ((r.v) - sqrt((r.v)**2 - v2*(r2-R2)))/v2
             */
 
-            Scalar mod_v = fast::sqrt(v2);
-            Scalar r_dot_v_hat = vr/mod_v;
-
-            const Scalar dx_ = r_dot_v_hat - fast::sqrt(r_dot_v_hat*r_dot_v_hat - r2 + R*R);
-            dt = dx_/mod_v;
+            Scalar rv = dot(pos,vel);
+            dt = (rv - fast::sqrt(rv*rv-v2*(r2-R2)))/v2;
 
             // backtrack the particle for time dt to get to point of contact
-            pos.x -= vel.x*dt;
-            pos.y -= vel.y*dt;
-            pos.z -= vel.z*dt;
+            pos -= vel*dt;
 
             // update velocity according to boundary conditions
+            /*
+             * Let n^ be the normal unit vector ar the point of contact.
+             * Therefore,
+             * v_perp = (v.n^)n^        -->         ((r.v)/R2)r         [R2, since the particle is on the surface]
+             * v_para = v-v_perp
+             */
             if (m_bc == boundary::no_slip)
                 {
-                // no-slip requires reflection of the tangential components.
-                // Radial component reflected since no penetration of the surface is necessary.
-                vel.x = -vel.x;
-                vel.y = -vel.y;
-                vel.z = -vel.z;
+                /* no-slip requires reflection of the tangential components.
+                 * Radial component reflected since no penetration of the surface is necessary.
+                 * This results in just flipping of all the velocity components.
+                 */
+                vel = -vel;
                 }
             else if (m_bc == boundary::slip)
                 {
-                // tangential components of the velocity is unchanged.
+                // tangential component of the velocity is unchanged.
                 // Radial component reflected since no penetration of the surface is necessary.
                 /*
                  * v' = -v_perp + v_para = v - 2*v_perp
                 */
-                r2 = dot(pos,pos);
-                vr = dot(pos,vel);
-                vel.x -= 2.0*vr*pos.x/r2;
-                vel.y -= 2.0*vr*pos.y/r2;
-                vel.z -= 2.0*vr*pos.z/r2;
+                const Scalar3 vperp = dot(vel,pos)*pos/R2;
+                vel -= Scalar(2)*vperp;
                 }
             return true;
             }
@@ -144,7 +132,7 @@ class __attribute__((visibility("default"))) SphereGeometry
          */
         HOSTDEVICE bool isOutside(const Scalar3& pos) const
             {
-            return dot(pos,pos) > R*R;
+            return dot(pos,pos) > R2;
             }
 
         //! Validate that the simulation box is large enough for the geometry
